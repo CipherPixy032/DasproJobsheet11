f3fc8d76a842a35bd1a63bc970ca0b79a6132782
CipherPixy032 <mollym3gtr@gmail.com>
2023-11-16 22:22:36 +0700

    Commit Experiment 2 no.5

--- Quiz.java
+++ Quiz.java
@@ -5,19 +5,25 @@ public class Quiz {
     public static void main(String[] args) {
         Random random = new Random();
         Scanner input = new Scanner(System.in);
-        char menu='y';
-        do{
+        char menu = 'y';
+        do {
             int number = random.nextInt(10) + 1;
             boolean success = false;
-            do{
-                System.out.print("Number Guessing (1-10): ");
+            do {
+                System.out.print("Guess a number (1-10): ");
                 int answer = input.nextInt();
                 input.nextLine();
                 success = (answer == number);
-            }while(!success);
-            System.out.print("Do you want to restart the game? (Y/y): ");
+                if (answer > number) {
+                    System.out.println("Too high");
+                } else if (answer < number) {
+                    System.out.println("Too low");
+                } else {
+                    System.out.println("Congrats!!!");
+                }
+            } while (!success);
+            System.out.print("Do you want to retry the game? (y/n): ");
             menu = input.nextLine().charAt(0);
-        }while(menu=='y'|| menu=='Y');
-
+        } while (menu == 'y' || menu == 'Y');
     }
 }
